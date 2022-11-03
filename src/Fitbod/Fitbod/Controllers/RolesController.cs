c24b62b681a6b5f3783a0fa81cbfd5bde66aebf6
Frederik Koch <40853139+FrederikHKoch@users.noreply.github.com>
2022-11-03 12:13:45 +0100

    added models and controller for user and role

---
+++ src/Fitbod/Fitbod/Controllers/RolesController.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fitbod.Data;
+using Fitbod.Models;
+
+namespace Fitbod.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly FitbodContext _context;
+
+        public RolesController(FitbodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Roles
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Role.ToListAsync());
+        }
+
+        // GET: Roles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Role == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Role
+                .FirstOrDefaultAsync(m => m.RoleId == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoleId,UserType")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(role);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Role == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Role.FindAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RoleId,UserType")] Role role)
+        {
+            if (id != role.RoleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(role);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoleExists(role.RoleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Role == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _context.Role
+                .FirstOrDefaultAsync(m => m.RoleId == id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Role == null)
+            {
+                return Problem("Entity set 'FitbodContext.Role'  is null.");
+            }
+            var role = await _context.Role.FindAsync(id);
+            if (role != null)
+            {
+                _context.Role.Remove(role);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoleExists(int id)
+        {
+          return _context.Role.Any(e => e.RoleId == id);
+        }
+    }
+}
---
+++ src/Fitbod/Fitbod/Data/FitbodContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Fitbod.Models;
+
+namespace Fitbod.Data
+{
+    public class FitbodContext : DbContext
+    {
+        public FitbodContext (DbContextOptions<FitbodContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fitbod.Models.User> User { get; set; } = default!;
+
+        public DbSet<Fitbod.Models.Role> Role { get; set; }
+    }
+}
