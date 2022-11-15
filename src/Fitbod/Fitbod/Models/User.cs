dcb799b5f36dfa57588efc12b71f1d91f283b606
Frederik Koch <40853139+FrederikHKoch@users.noreply.github.com>
2022-11-15 12:22:33 +0100

    Rollback if login doesnt work

--- src/Fitbod/Fitbod/Models/User.cs
+++ src/Fitbod/Fitbod/Models/User.cs
@@ -10,7 +10,6 @@ public class User
     [Required][RegularExpression(@"^[a-zA-Z\s]{1,40}$", ErrorMessage = "Numbers and special characters are not allowed")] public string FirstName { get; set; }
     [Required][RegularExpression(@"^[a-zA-Z\s]{1,40}$", ErrorMessage = "Numbers and special characters are not allowed")] public string LastName { get; set; }
     [Required][EmailAddress] public string Email { get; set; }
-    public string Gender { get; set; }
     [Required][PasswordPropertyText][StringLength(40, ErrorMessage = "Password must be between 6 and 40 characters", MinimumLength=6)] public string Password { get; set; }
     public int RoleId { get; set; }
     [ForeignKey("RoleId")] public Role Role { get; set; }
