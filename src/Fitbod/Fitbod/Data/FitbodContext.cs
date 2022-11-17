5715f52ce953f713ec2e9c6bcea68e31e0cd9568
Leroikier <42372446+Leroikier@users.noreply.github.com>
2022-11-17 10:23:30 +0100

    Alexander branch (#43)
    * Test Commit/pull
    
    * First commit
    
    * Removed User model and stuff since it was just a test
    
    * Added some changes to Startpage
    
    * Almost done with homepage
    
    * Delete appsettings.json
    
    * Done with layout page and homepage and contact us page.
    
    * Done with Homepage and Conntact us And some model change for Trainingclass
    
    * Added seeding of database, Exercise and Role is seeded

--- src/Fitbod/Fitbod/Data/FitbodContext.cs
+++ src/Fitbod/Fitbod/Data/FitbodContext.cs
@@ -6,16 +6,23 @@ using Fitbod.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Fitbod.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Fitbod.Data
 {
     public class FitbodContext : IdentityDbContext<FitbodUser>
     {
-        public FitbodContext (DbContextOptions<FitbodContext> options)
+        public FitbodContext(DbContextOptions<FitbodContext> options)
             : base(options)
         {
         }
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            new DbInitializer(modelBuilder).Seed();
+        }
+
         public DbSet<Exercise> Exercise { get; set; }
 
         public DbSet<ExercisePlan> ExercisePlan { get; set; }
@@ -33,5 +40,41 @@ namespace Fitbod.Data
         public DbSet<Fitbod.Models.TrainingClass> TrainingClass { get; set; }
         public DbSet<Areas.Identity.Data.FitbodUser> FitbodUser { get; set; }
 
+    }
+
+    public class DbInitializer
+    {
+        private readonly ModelBuilder modelBuilder;
+
+        public DbInitializer(ModelBuilder modelBuilder)
+        {
+            this.modelBuilder = modelBuilder;
+        }
+
+        public void Seed()
+        {
+            modelBuilder.Entity<Exercise>().HasData(
+                   new Exercise() { ExerciseId = 1, Name = "Squat", Musclegroup = "Baglår", Description = "Adddescriptionhere", Image = "Squat.img" },
+                   new Exercise() { ExerciseId = 2, Name = "Deadlifts", Musclegroup = "Baglår", Description = "Adddescriptionhere", Image = "Deadlift.img" },
+                   new Exercise() { ExerciseId = 3, Name = "Jump rope", Musclegroup = "Læg", Description = "Adddescriptionhere", Image = "Jump.img" },
+                   new Exercise() { ExerciseId = 4, Name = "Dumbbell jump squat", Musclegroup = "Læg", Description = "Adddescriptionhere", Image = "jump2.img" },
+                   new Exercise() { ExerciseId = 5, Name = "Bench press", Musclegroup = "Bryst", Description = "Adddescriptionhere", Image = "benchpress.img" },
+                   new Exercise() { ExerciseId = 6, Name = "Seated Cable Chest Press", Musclegroup = "Bryst", Description = "Adddescriptionhere", Image = "vroom.img" },
+                   new Exercise() { ExerciseId = 7, Name = "Chest-supported Row", Musclegroup = "Ryg", Description = "Adddescriptionhere", Image = "row.img" },
+                   new Exercise() { ExerciseId = 8, Name = "Pull-ups", Musclegroup = "Ryg", Description = "Adddescriptionhere", Image = "pullup.img" },
+                   new Exercise() { ExerciseId = 9, Name = "Overhead press", Musclegroup = "Skulder", Description = "Adddescriptionhere", Image = "shoulder.img" },
+                   new Exercise() { ExerciseId = 10, Name = "Reverse grip", Musclegroup = "Triceps", Description = "Adddescriptionhere", Image = "reverse.img" },
+                   new Exercise() { ExerciseId = 11, Name = "Rope Triceps Pressdown", Musclegroup = "Triceps", Description = "Adddescriptionhere", Image = "trex.img" },
+                   new Exercise() { ExerciseId = 12, Name = "Close grip pull-up", Musclegroup = "Biceps", Description = "Adddescriptionhere", Image = "closedgrippullup.img" },
+                   new Exercise() { ExerciseId = 13, Name = "Dumbbell Curls", Musclegroup = "Biceps", Description = "Adddescriptionhere", Image = "bellcurls.img" },
+                   new Exercise() { ExerciseId = 14, Name = "Decline Crunch", Musclegroup = "Mavemuskler", Description = "Adddescriptionhere", Image = "crunch.img" }
+            );
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole() { Id = Guid.NewGuid().ToString(), Name = "Bruger", NormalizedName = "BRUGER" },
+                new IdentityRole() { Id = Guid.NewGuid().ToString(), Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole() { Id = Guid.NewGuid().ToString(), Name = "Superbruger", NormalizedName = "SUPERBRUGER" }
+            );
+        }
     }
 }
